3e7af4b16603ceedbea5a6e50f77fad0d2850978
Nishat <104446439+zas-nishat@users.noreply.github.com>
2025-10-26 14:28:56 +0600

    deployment success

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import personalInfo from "../data/personalinfo.json";
-import { Github, Linkedin, Facebook, Heart } from "lucide-react";
+import { Github, Linkedin, Facebook, Heart, Rocket, Wind } from "lucide-react";
 
 const socialIcons: { [key: string]: React.ElementType } = {
   github: Github,
@@ -119,6 +119,18 @@ const Footer: React.FC = () => {
           © {currentYear} {personalInfo.name}. All rights reserved.
         </p>
 
+        {/* Right Side - Made With */}
+        <div className="flex items-center space-x-2 text-gray-500 dark:text-gray-400">
+          <span>Made with</span>
+          <Heart
+            size={16}
+            className="text-red-500 animate-pulse"
+            fill="currentColor"
+          />
+          <span>using</span>
+          <Rocket className="text-sky-500" size={18} />
+          <Wind className="text-teal-400" size={18} />
+        </div>
       </div>
     </footer>
   );
